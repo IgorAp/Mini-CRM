a3174e69890cf8fa47155f672c1bded687a3e1cb
igor <igor@sememail.com>
2018-04-01 15:37:10 -0300

    coding of the company model and creation of control company

--- app/Company.php
+++ app/Company.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'email',
+        'logo',
+        'website'
+    ];
+    public static function rules(){
+        $rules = [
+            'name'=>'required',
+            'img'=>'dimensions:min_width=100,min_height=100'
+        ];
+        return $rules;
+    }
 }
